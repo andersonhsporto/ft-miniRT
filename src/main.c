932cd09d61b289643656698e937788f89bfea3c5
Alef <alefff.gabriel@gmail.com>
2022-04-09 21:41:14 -0300

    project structure

---
+++ src/main.c
@@ -0,0 +1,21 @@
+#include "../include/mlx.h"
+
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	//my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	mlx_loop(mlx);
+}
