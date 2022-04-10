b9c8eb5e31465c52722d25e04670b9d1308f13a1
Alef <alefff.gabriel@gmail.com>
2022-04-10 15:28:22 -0300

    starting in rgb colors

--- src/main.c
+++ src/main.c
@@ -8,13 +8,35 @@ typedef struct	s_data {
 	int		endian;
 }				t_data;
 
+static void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
 int	main(void)
 {
 	void	*mlx;
 	void	*mlx_win;
 	t_data	img;
+	int		color;
+	int		h = 100;
+	int		w = 200;
 
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 200, 100, "Hello world!");
+	img.img = mlx_new_image(mlx, w, h);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	mlx_win = mlx_new_window(mlx, w, h, "Hello world!");
+	for (int y = h - 1; y != 0; y--)
+	{
+		for (int x = 0; x < w; x++)
+		{
+			color = (x*255)/w+((((w-x)*255)/w)<<16)+(((y*255)/h)<<8);
+			my_mlx_pixel_put(&img, x, y, color);
+		}
+	}
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 }
