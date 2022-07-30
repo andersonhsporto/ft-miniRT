a7984d303aa1a937d32bd927fb2c3620601a9893
Anderson Porto <anderson.higo2@gmail.com>
2022-07-30 00:34:21 -0300

    add: file function

--- src/files/1_file/0_check_file.c
+++ src/files/1_file/0_check_file.c
@@ -5,14 +5,16 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: anhigo-s <anhigo-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/18 22:28:43 by anhigo-s          #+#    #+#             */
-/*   Updated: 2022/07/28 23:13:24 by anhigo-s         ###   ########.fr       */
+/*   Created: 2022/07/29 13:25:56 by anhigo-s          #+#    #+#             */
+/*   Updated: 2022/07/30 00:32:17 by anhigo-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
 static int	check_line(char *line);
+static void	exit_and_free(t_mini *data);
+static void	check_legal_char(char *file, t_mini *data);
 
 void	check_file(t_mini *data, char *file)
 {
@@ -20,25 +22,58 @@ void	check_file(t_mini *data, char *file)
 	char	*temp;
 	char	*map_line;
 
+	check_legal_char(file, data);
 	fd = open(file, O_RDONLY);
-	if (fd < 0)
-	{
-		print_error("miniRT: Open Error");
-		exit (1);
-	}
 	while (fd)
 	{
 		map_line = get_next_line(fd);
-		if (map_line == NULL || check_line(map_line))
+		if (map_line == NULL)
 			break ;
 		temp = replace_string(map_line, '+');
 		get_line_scene(data, temp);
 		free(temp);
 	}
 	close(fd);
+	data->light->intensity = init_light_intensity(&data->light_a->rgb, \
+												data->light_a->ratio);
 	debug(data);
-	data->light->intensity = init_light_intensity(&data->light_a->rgb, data->light_a->ratio);
+}
+
+static void	check_legal_char(char *file, t_mini *data)
+{
+	char	*line;
+	int		fd;
+
+	fd = open(file, O_RDONLY);
+	if (fd < 0)
+		exit_and_free(data);
+	while (fd)
+	{
+		line = get_next_line(fd);
+		if (line == NULL)
+			break ;
+		else if (check_line(line))
+		{
+			print_error("miniRT: Illegal Character");
+			close(fd);
+			free(line);
+			exit_and_free(data);
+		}
+		free(line);
+	}
+	close(fd);
 	return ;
+}
+
+static void	exit_and_free(t_mini *data)
+{
+	print_error("miniRT: Open Error");
+	mlx_destroy_image(data->mlx->mlx, data->img.img);
+	mlx_destroy_window(data->mlx->mlx, data->mlx->win);
+	mlx_destroy_display(data->mlx->mlx);
+	free(data->mlx->mlx);
+	free(data->mlx);
+	exit(1);
 }
 
 static int	check_line(char *line)
@@ -50,12 +85,9 @@ static int	check_line(char *line)
 	{
 		if (!legal_char(line[index]) && index > 1)
 		{
-			print_error("miniRT: Illegal Character");
-			exit(1); //free memory
 			return (1);
 		}
 		index++;
 	}
 	return (0);
 }
-
