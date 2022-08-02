2f58a3b43f4a6cd7893b71e71475a985419783b2
Alef <alefff.gabriel@gmail.com>
2022-08-02 00:56:38 -0300

    Merge branch 'main' of github.com:andersonhsporto/ft-miniRT into main

--- src/includes/minirt.h
+++ src/includes/minirt.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minirt.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anhigo-s <anhigo-s@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: algabrie <alefgabrielr@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/18 22:26:07 by anhigo-s          #+#    #+#             */
-/*   Updated: 2022/08/01 22:56:50 by anhigo-s         ###   ########.fr       */
+/*   Updated: 2022/08/02 00:48:23 by algabrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@
 #include <math.h>
 #include <string.h> // TODO VERIFICAR SE É REALMENTE NECESSARIO
 
-#define WIDTH 10
-#define HEIGHT 10
+#define WIDTH 600
+#define HEIGHT 600
 
 #define EPSILON 0.00001
 
